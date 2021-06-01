bd2039411d5b4f91a76742af546e5066e18b52d3
unknown <immonirulfahad@gmail.com>
2021-06-01 09:48:11 +0600

    day 11

---
+++ 11-day-event-keycord/script.js
@@ -0,0 +1,17 @@
+window.addEventListener('keydown', (e) => {
+    insert.innerHTML = `
+    <div class="key">
+    ${event.key === ' ' ? 'space' : event.key}
+    <small>event.key</small>
+</div>
+
+<div class="key">
+    ${event.keyCode}
+    <small>event.key</small>
+</div>
+
+<div class="key">
+    ${event.code}
+    <small>event.code</small>
+</div>`
+})
